dc6d10a9f1a16c890efe26075b5bc6b0ce244bf6
Everton Rodrigo Tefili <tefili1978@yahoo.com.br>
2021-09-07 17:22:42 -0300

    sending Angular frontend project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent,
+    UpdateRoomComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
